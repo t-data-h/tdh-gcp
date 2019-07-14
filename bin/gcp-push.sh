9fa50e8a0020d3afa57254fe40e300ef0d8741b7
tca <tcarland@gmail.com>
2019-07-14 10:44:23 -0700

    put back show slave status usage for gcp-push

--- bin/gcp-push.sh
+++ bin/gcp-push.sh
@@ -4,32 +4,36 @@
 PNAME=${0##*\/}
 DISTPATH="${HOME}/tmp/dist"
 
+APATH=$( realpath $1 )
+ANAME="$2"
+GCPHOST="$3"
+
 
 usage()
 {
     echo ""
-    echo "$PNAME  [path] <archive_name> <gcphost>"
+    echo "$PNAME [path] <archive_name> <gcphost>"
     echo ""
     echo "  Where 'path' is the directory to be archived."
-    echo " The script assumes that the archive will contain the final"
+    echo ""
+    echo "   The script assumes that the archive will contain the final"
     echo " directory, so a path of a '/a/b/c' will create the archive from 'b'"
     echo " with the tarfile containing './c/' as the root directory"
     echo ""
-    echo "  Archive name is an altername name to call the tarball. The value"
-    echo " 'foo' will result in an archive of foo.tar.gz"
+    echo "   Archive name is an altername name to call the tarball. The"
+    echo " value 'somepkg' will result in an archive of somepkg.tar.gz"
+    echo " By default, the archive will be named after the final directory."
     echo ""
     echo "  The environment variable 'GCH_PUSH_HOST' is honored as the "
-    echo " the default 'gcphost' to use."
+    echo " the default 'gcphost' to use. If not set, all three parameters"
+    echo " are required."
     echo ""
 }
+
 
 if [ -n "$GCP_DIST_PATH" ]; then
     DISTPATH="$GCP_DIST_PATH"
 fi
-
-APATH=$( realpath $1 )
-ANAME="$2"
-GCPHOST="$3"
 
 if [ -z "$GCPHOST" ]; then
     GCPHOST="$GCP_PUSH_HOST"
@@ -58,6 +62,7 @@ cd $target
 
 ( tar -cvf ${DISTPATH}/${ANAME}.tar ./${name} )
 ( gzip ${DISTPATH}/${ANAME}.tar )
+( gcloud compute ssh ${GCPHOST} --command "mkdir -p /tmp/dist" )
 ( gcloud compute scp ${DISTPATH}/${ANAME}.tar.gz ${GCPHOST}:/tmp/dist/ )
 
 rt=$?
@@ -67,4 +72,5 @@ fi
 
 ( rm ${DISTPATH}/${ANAME}.tar.gz )
 
+echo "$PNAME Finished."
 exit $rt
