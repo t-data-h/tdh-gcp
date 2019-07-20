4406f84dcb14e5dd5b87436626cc7885e753411c
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-07-20 15:05:54 -0700

    V0.8x (#11)
    * readme updates
    * add tags for config, mgr and python only runs
    * add version and dryrun options for wrapper scripts
    * fix mgr tag in wrapper
    * tag hive mysql pw reset for config updates

---
+++ ansible/bin/tdh-python-update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#  Install wrapper script to sync configs only
+#
+PNAME=${0##*\/}
+tdh_path=$(dirname "$(readlink -f "$0")")
+
+TDH_ANSIBLE_HOME=$(dirname $tdh_path)
+ACTION="$1"
+rt=0
+
+# distribute
+echo "Running Ansible Playbooks : tdh-distribute, tdh_install --tags tdh-python"
+echo "TDH_ANSIBLE_HOME = '$TDH_ANSIBLE_HOME'"
+
+if [ -z "$ACTION" ]; then
+    echo ""
+    echo "Usage: $PNAME <action> "
+    echo "  any action other than 'run' is a 'dryrun'"
+    echo ""
+    exit 0
+fi
+
+cd $TDH_ANSIBLE_HOME
+
+echo "( ansible-playbook -i inventory/tdh-west1/hosts tdh-distribute.yml )"
+if [[ $ACTION == "run" ]]; then
+    ( ansible-playbook -i inventory/tdh-west1/hosts tdh-distribute.yml )
+    rt=$?
+fi
+
+if [ $rt -gt 0 ]; then
+    echo "$PNAME: Error in Distribute, Aborting."
+    exit $rt
+fi
+
+# install
+echo "( ansible-playbook -i inventory/tdh-west1/hosts --tags tdh-python tdh-install.yml )"
+if [[ $ACTION == "run" ]]; then
+    ( ansible-playbook -i inventory/tdh-west1/hosts --tags tdh-python tdh-install.yml )
+    rt=$?
+fi
+
+exit $rt
