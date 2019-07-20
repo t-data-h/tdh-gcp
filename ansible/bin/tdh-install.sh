b2b8d659c5f34be6c737be199bc8aaef02db1aa4
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-07-20 15:05:54 -0700

    V0.8x (#11)
    * readme updates
    * add tags for config, mgr and python only runs
    * add version and dryrun options for wrapper scripts
    * fix mgr tag in wrapper
    * tag hive mysql pw reset for config updates

--- ansible/bin/tdh-install.sh
+++ ansible/bin/tdh-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 #  Install wrapper script to distribute packages and run the installation
 #
 PNAME=${0##*\/}
@@ -6,24 +7,48 @@ tdh_path=$(dirname "$(readlink -f "$0")")
 
 TDH_ANSIBLE_HOME=$(dirname $tdh_path)
 ACTION="$1"
+dryrun=1
 rt=0
 
-# distribute
-echo "Running Ansible Playbooks : tdh-distribute, tdh_install"
-echo "TDH_ANSIBLE_HOME = '$TDH_ANSIBLE_HOME'"
+# -------
+
+if [ -e $TDH_ANSIBLE_HOME/../bin/tdh-gcp-config.sh ]; then
+    . $TDH_ANSIBLE_HOME/../bin/tdh-gcp-config.sh
+fi
+
 
 if [ -z "$ACTION" ]; then
     echo ""
     echo "Usage: $PNAME <action> "
-    echo "  any action other than 'run' does nothing"
+    echo "  any action other than 'run' is a 'dryrun'"
     echo ""
     exit 0
 fi
 
+# -------
+
+case "$ACTION" in
+    -V|--version)
+        echo "$PNAME  (tdh-gcp)  v$TDH_GCP_VERSION"
+        exit 1
+        ;;
+    run)
+        dryrun=0
+        ;;
+    *)
+        ;;
+esac
+
 cd $TDH_ANSIBLE_HOME
 
+echo "Running Ansible Playbooks : tdh-distribute, tdh_install"
+echo "TDH_ANSIBLE_HOME = '$TDH_ANSIBLE_HOME'"
+echo "" 
+
+# ------- Distribute
+
 echo "( ansible-playbook -i inventory/tdh-west1/hosts tdh-distribute.yml )"
-if [[ $ACTION == "run" ]]; then
+if [[ $dryrun -eq 0 ]]; then
     ( ansible-playbook -i inventory/tdh-west1/hosts tdh-distribute.yml )
     rt=$?
 fi
@@ -33,9 +58,10 @@ if [ $rt -gt 0 ]; then
     exit $rt
 fi
 
-# install
+# ------- Install
+
 echo "( ansible-playbook -i inventory/tdh-west1/hosts tdh-install.yml )"
-if [[ $ACTION == "run" ]]; then
+if [[ $dryrun -eq 0 ]]; then
     ( ansible-playbook -i inventory/tdh-west1/hosts tdh-install.yml )
     rt=$?
 fi
