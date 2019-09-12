6edada2173137b2929487ad136db648a91c9ab48
tca <tcarland@gmail.com>
2019-09-12 09:40:19 -0700

    sep master_id for clarity

--- bin/tdh-masters-init.sh
+++ bin/tdh-masters-init.sh
@@ -18,10 +18,11 @@ zone=
 mtype="$GCP_DEFAULT_MACHINETYPE"
 bootsize="$GCP_DEFAULT_BOOTSIZE"
 disksize="$GCP_DEFAULT_DISKSIZE"
-master_id="master-id_rsa.pub"
-master_id_file="${tdh_path}/../ansible/.ansible/${master_id}"
 network="tdh-net"
 subnet="tdh-net-west1"
+
+master_id="master-id_rsa.pub"
+master_id_file="${tdh_path}/../ansible/.ansible/${master_id}"
 
 myid=1
 dryrun=0
