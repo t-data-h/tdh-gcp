1a3f783df9aa71bfdfca1228e690a9e92b9db85b
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-11-14 12:04:40 -0800

    V1.0.6 (#35)
    * fix lab inventory
    * add xfs support and corresponding --force options

--- bin/tdh-masters-init.sh
+++ bin/tdh-masters-init.sh
@@ -17,6 +17,7 @@ zone=
 mtype="$GCP_DEFAULT_MACHINETYPE"
 bootsize="$GCP_DEFAULT_BOOTSIZE"
 disksize="$GCP_DEFAULT_DISKSIZE"
+format="$TDH_FORMAT"
 network=
 subnet=
 
@@ -306,16 +307,16 @@ for name in $names; do
         device="/dev/sdb"
         mountpoint="/data"
         echo " -> Attaching disk"
-        echo "( $GSSH $host --command './tdh-gcp-format.sh $device $mountpoint' )"
+        echo "( $GSSH $host --command './${format} -f $device $mountpoint' )"
         if [ $dryrun -eq 0 ]; then
-            ( $GSCP ${tdh_path}/tdh-gcp-format.sh ${host}: )
-            ( $GSSH $host --command 'chmod +x tdh-gcp-format.sh' )
-            ( $GSSH $host --command "./tdh-gcp-format.sh $device $mountpoint" )
+            ( $GSCP ${tdh_path}/${format} ${host}: )
+            ( $GSSH $host --command "chmod +x $format" )
+            ( $GSSH $host --command "./$format -f $device $mountpoint" )
         fi
 
         rt=$?
         if [ $rt -gt 0 ]; then
-            echo "Error in tdh-gcp-format for $host"
+            echo "Error in $format for $host"
             break
         fi
     fi
